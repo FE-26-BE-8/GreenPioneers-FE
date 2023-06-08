f5ada2183834df9f0fcf78cb50c7b6acf19285dd
KhathirulSan <paturihsan4@gmail.com>
2023-06-08 22:01:30 +0700

    Menambahkan UseState

--- src/components/pages/news/News.jsx
+++ src/components/pages/news/News.jsx
@@ -1,189 +1,112 @@
 import "./News.css";
+import React, { useState } from 'react';
+
 function News() {
+  const [newsData, setNewsData ]  = useState([
+    {
+      id: 1,
+      title: 'Penyebab Terjadinya Kebakaran Di Indonesia #1',
+      description: 'Penyebab kebakaran hutan di Indonesia Pada dasar nya memiliki dua penyebab, yaitu kebakaran yang disebabkan oleh faktor alam dan juga faktor manusia namun sebagaian besar kebakaran hutan yang terjadi di Indonesia belakangan ini disebabkan karena ulah dari manusia nya sendiri...',
+      imageSrc: 'https://i.ibb.co/M8XhDK3/image-2023-06-05-122457381.png',
+    },
+    {
+      id: 2,
+      title: 'Penyebab Terjadinya Kebakaran Di Indonesia #2',
+      description: 'Penyebab kebakaran hutan di Indonesia Pada dasar nya memiliki dua penyebab, yaitu kebakaran yang disebabkan oleh faktor alam dan juga faktor manusia namun sebagaian besar kebakaran hutan yang terjadi di Indonesia belakangan ini disebabkan karena ulah dari manusia nya sendiri...',
+      imageSrc: 'https://i.ibb.co/M8XhDK3/image-2023-06-05-122457381.png',
+    },
+    {
+      id: 3,
+      title: 'Penyebab Terjadinya Kebakaran Di Indonesia #3',
+      description: 'Penyebab kebakaran hutan di Indonesia Pada dasar nya memiliki dua penyebab, yaitu kebakaran yang disebabkan oleh faktor alam dan juga faktor manusia namun sebagaian besar kebakaran hutan yang terjadi di Indonesia belakangan ini disebabkan karena ulah dari manusia nya sendiri...',
+      imageSrc: 'https://i.ibb.co/M8XhDK3/image-2023-06-05-122457381.png',
+    },
+    {
+      id: 4,
+      title: 'Penyebab Terjadinya Kebakaran Di Indonesia #4',
+      description: 'Penyebab kebakaran hutan di Indonesia Pada dasar nya memiliki dua penyebab, yaitu kebakaran yang disebabkan oleh faktor alam dan juga faktor manusia namun sebagaian besar kebakaran hutan yang terjadi di Indonesia belakangan ini disebabkan karena ulah dari manusia nya sendiri...',
+      imageSrc: 'https://i.ibb.co/M8XhDK3/image-2023-06-05-122457381.png',
+    },
+  ]);
+
+  const [topics, setTopics] = useState([
+    {
+      title: 'Penyebab Terjadinya Kebakaran Di Indonesia',
+      image: 'https://i.ibb.co/M8XhDK3/image-2023-06-05-122457381.png',
+      updatedAt: 'Last updated 3 mins ago',
+    },
+    {
+      title: 'Dampak Aktivitas Bakar Sampah di Jabodetabek',
+      image: 'https://i.ibb.co/M8XhDK3/image-2023-06-05-122457381.png',
+      updatedAt: 'Last updated 3 mins ago',
+    },
+    {
+      title: 'Dampak Pemanasan Global',
+      image: 'https://i.ibb.co/M8XhDK3/image-2023-06-05-122457381.png',
+      updatedAt: 'Last updated 3 mins ago',
+    },
+    {
+      title: 'Kenapa Polutan Adalah Zat yang Harus Kita Hindari?',
+      image: 'https://i.ibb.co/M8XhDK3/image-2023-06-05-122457381.png',
+      updatedAt: 'Last updated 3 mins ago',
+    },
+  ]);
+
   return (
-    <>
+   <>
+      {/* //---------------------NEWS-------------------------- */}
+
       <div className="news-container container">
-        <div className="col-md-7">
-          <h1 className="heading">News</h1>
-          <div class="card mb-3 p-4 news-card">
-            <div class="row g-0">
-              <div class="col-md-6">
-                <h5 class="card-title">
-                  Penyebab Terjadinya Kebakaran Di Indonesia #1
-                </h5>
-                <p class="card-text">
-                  Penyebab kebakaran hutan di Indonesia Pada dasar nya memiliki
-                  dua penyebab, yaitu kebakaran yang disebabkan oleh faktor alam
-                  dan juga faktor manusia namun sebagaian besar kebakaran hutan
-                  yang terjadi di Indonesia belakangan ini disebabkan karena
-                  ulah dari manusia nya sendiri...
-                </p>
-                <div class="text-center btn-article">
+      <div className="col-md-7">
+        <h1 className="heading">News</h1>
+        {newsData.map((news) => (
+          <div className="card mb-3 p-4 news-card" key={news.id}>
+            <div className="row g-0">
+              <div className="col-md-6">
+                <h5 className="card-title">{news.title}</h5>
+                <p className="card-text">{news.description}</p>
+                <div className="text-center btn-article">
                   <button className="btn-news">Baca Selengkapnya</button>
                 </div>
               </div>
-              <div class="col-md-6">
-                <div class="card-body">
+              <div className="col-md-6">
+                <div className="card-body">
                   <img
-                    src="https://i.ibb.co/M8XhDK3/image-2023-06-05-122457381.png"
+                    src={news.imageSrc}
                     alt="Kebakaran Hutan"
-                    class="img-fluid img-news"
+                    className="img-fluid img-news"
                   />
                 </div>
               </div>
             </div>
           </div>
-          <div class="card mb-3 p-4 news-card">
-            <div class="row g-0">
-              <div class="col-md-6">
-                <h5 class="card-title">
-                  Penyebab Terjadinya Kebakaran Di Indonesia #2
-                </h5>
-                <p class="card-text">
-                  Penyebab kebakaran hutan di Indonesia Pada dasar nya memiliki
-                  dua penyebab, yaitu kebakaran yang disebabkan oleh faktor alam
-                  dan juga faktor manusia namun sebagaian besar kebakaran hutan
-                  yang terjadi di Indonesia belakangan ini disebabkan karena
-                  ulah dari manusia nya sendiri...
-                </p>
-                <div class="text-center btn-article">
-                  <button className="btn-news">Baca Selengkapnya</button>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="card-body">
-                  <img
-                    src="https://i.ibb.co/M8XhDK3/image-2023-06-05-122457381.png"
-                    alt="Kebakaran Hutan"
-                    class="img-fluid img-news"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="card mb-3 p-4 news-card">
-            <div class="row g-0">
-              <div class="col-md-6">
-                <h5 class="card-title">
-                  Penyebab Terjadinya Kebakaran Di Indonesia #3
-                </h5>
-                <p class="card-text">
-                  Penyebab kebakaran hutan di Indonesia Pada dasar nya memiliki
-                  dua penyebab, yaitu kebakaran yang disebabkan oleh faktor alam
-                  dan juga faktor manusia namun sebagaian besar kebakaran hutan
-                  yang terjadi di Indonesia belakangan ini disebabkan karena
-                  ulah dari manusia nya sendiri...
-                </p>
-                <div class="text-center btn-article">
-                  <button className="btn-news">Baca Selengkapnya</button>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="card-body">
-                  <img
-                    src="https://i.ibb.co/M8XhDK3/image-2023-06-05-122457381.png"
-                    alt="Kebakaran Hutan"
-                    class="img-fluid img-news"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="card mb-3 p-4 news-card">
-            <div class="row g-0">
-              <div class="col-md-6">
-                <h5 class="card-title">
-                  Penyebab Terjadinya Kebakaran Di Indonesia #4
-                </h5>
-                <p class="card-text">
-                  Penyebab kebakaran hutan di Indonesia Pada dasar nya memiliki
-                  dua penyebab, yaitu kebakaran yang disebabkan oleh faktor alam
-                  dan juga faktor manusia namun sebagaian besar kebakaran hutan
-                  yang terjadi di Indonesia belakangan ini disebabkan karena
-                  ulah dari manusia nya sendiri...
-                </p>
-                <div class="text-center btn-article">
-                  <button className="btn-news">Baca Selengkapnya</button>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="card-body">
-                  <img
-                    src="https://i.ibb.co/M8XhDK3/image-2023-06-05-122457381.png"
-                    alt="Kebakaran Hutan"
-                    class="img-fluid img-news"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-4 mt-lg-5 trend-container">
-          <div class="trending-topics">
-            <h2>Trending Topics</h2>
-            <ul>
-              <li>
-                <div class="topic">
-                  <img src="https://i.ibb.co/M8XhDK3/image-2023-06-05-122457381.png" />
-                  <div>
-                    <a href="#">Penyebab Terjadinya Kebakaran Di Indonesia</a>
-                    <p class="card-text">
-                      <small class="text-body-secondary">
-                        Last updated 3 mins ago
-                      </small>
-                    </p>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="topic">
-                  <img src="https://i.ibb.co/M8XhDK3/image-2023-06-05-122457381.png" />
-                  <div>
-                    <a href="#">
-                      Dampak Aktivitas Bakar Sampah di Jabodetabek{" "}
-                    </a>
-                    <p class="card-text">
-                      <small class="text-body-secondary">
-                        Last updated 3 mins ago
-                      </small>
-                    </p>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="topic">
-                  <img src="https://i.ibb.co/M8XhDK3/image-2023-06-05-122457381.png" />
-                  <div>
-                    <a href="#">Dampak Pemanasan Global </a>
-                    <p class="card-text">
-                      <small class="text-body-secondary">
-                        Last updated 3 mins ago
-                      </small>
-                    </p>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="topic">
-                  <img src="https://i.ibb.co/M8XhDK3/image-2023-06-05-122457381.png" />
-                  <div>
-                    <a href="#">
-                      Kenapa Polutan Adalah Zat yang Harus Kita Hundari ?
-                    </a>
-                    <p class="card-text">
-                      <small class="text-body-secondary">
-                        Last updated 3 mins ago
-                      </small>
-                    </p>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
+        ))}
       </div>
+      {/* //---------------------Trending Topics-------------------------- */}
+      <div className="col-md-4 mt-lg-5 trend-container">
+      <div className="trending-topics">
+        <h2>Trending Topics</h2>
+        <ul>
+          {topics.map((topic, index) => (
+            <li key={index}>
+              <div className="topic">
+                <img src={topic.image} alt="Trending Topic" />
+                <div>
+                  <a href="#">{topic.title}</a>
+                  <p className="card-text">
+                    <small className="text-body-secondary">
+                      {topic.updatedAt}
+                    </small>
+                  </p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+    </div>
+      
     </>
   );
 }
