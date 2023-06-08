e63e7679dbe1dec5229555736624a989e1df727f
Muhammad Iqbal <76693706+muhiiqbal@users.noreply.github.com>
2023-06-09 01:00:55 +0700

    penambahan state

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,30 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import '../css/hubungi.css';
 
 const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="container mt-5">
       <h1>Hubungi Kami</h1>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group controlId="formName">
           <Form.Label>Nama</Form.Label>
-          <Form.Control type="text" placeholder="Masukkan nama Anda" />
+          <Form.Control
+            type="text"
+            placeholder="Masukkan nama Anda"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group controlId="formEmail">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Masukkan email Anda" />
+          <Form.Control
+            type="email"
+            placeholder="Masukkan email Anda"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group controlId="formSubject">
           <Form.Label>Subject</Form.Label>
-          <Form.Control type="text" placeholder="Masukkan subjek pesan" />
+          <Form.Control
+            type="text"
+            placeholder="Masukkan subjek pesan"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group controlId="formMessage">
           <Form.Label>Pesan</Form.Label>
-          <Form.Control as="textarea" rows={4} placeholder="Masukkan pesan Anda" />
+          <Form.Control
+            as="textarea"
+            rows={4}
+            placeholder="Masukkan pesan Anda"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
         </Form.Group>
 
         <div className="form-divider"></div>
@@ -37,6 +67,6 @@ const ContactForm = () => {
       </Form>
     </div>
   );
-}
+};
 
 export default ContactForm;
