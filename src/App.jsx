fcfc765b89085f2c06acc3b8d1bea015fe672e8a
Muhammad Iqbal <76693706+muhiiqbal@users.noreply.github.com>
2023-06-09 00:54:37 +0700

    perubahan sedikit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './redux';
 import Register from './components/Register';
-
-const store = createStore(rootReducer);
 
 const App = () => {
   return (
-    <Provider store={store}>
+    <>
       <Register />
-    </Provider>
+    </>
   );
-};
+}
 
 export default App;
