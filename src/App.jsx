755af24732b8e786b9ced32edd0b289911ece115
Muhammad Iqbal <76693706+muhiiqbal@users.noreply.github.com>
2023-06-09 00:19:47 +0700

    Penambahan

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import './App.css';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import ContactInfo from './components/ContactInfo';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <div className="container mt-5">
-        <h1>Contact Us</h1>
-        <ContactInfo />
-      </div>
-    </Provider>
+    <>
+      <h1>Contact Us</h1>
+      <ContactInfo />
+    </>
   );
 }
