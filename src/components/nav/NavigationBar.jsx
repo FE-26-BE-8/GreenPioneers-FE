cae344fe643f5f21e03a007153f2f55b05094e2b
KhathirulSan <paturihsan4@gmail.com>
2023-06-10 16:16:32 +0700

    Merapihkan navbar

--- src/components/nav/NavigationBar.jsx
+++ src/components/nav/NavigationBar.jsx
@@ -1,45 +1,53 @@
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import React from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+
+import "./Navbar.css";
+
 function NavigationBar() {
   return (
-    <>
-      <Navbar className="container-nav" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="#">
+    <Navbar className="container-nav" expand="lg">
+      <Container fluid>
+        <Navbar.Brand>
           <div className="nav-logo">
-          <div className="logo-name1">
-            <a href="#">Green</a>
-          </div>
-          <div className="logo-name2">
-            <a href="#">Pioneers</a>
-          </div>
-        </div>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="m-auto my-lg-0 wrapper-navlist"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-         
-              <Link to ="/" className="m-auto nav-list">Home</Link>
-              <Link to ="/list-news" className="m-auto nav-list">News</Link>
-              <Link to ="/list-tips" className="m-auto nav-list">Tips</Link>
-              <Nav.Link href="#action1"  className="m-auto nav-list">Program</Nav.Link>
-            </Nav>
-            <div className="d-flex gap-2 btn-nav">
-              <Button variant="outline-success" className="w-100 btn-login">Login</Button>
-              <Button variant="outline-success" className="w-100 btn-register">Register</Button>
+            <div className="logo-name1">
+              <a href="#">Green</a>
             </div>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+            <div className="logo-name2">
+              <a href="#">Pioneers</a>
+            </div>
+          </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="mx-auto my-lg-0 wrapper-navlist" navbarScroll>
+            <Link to="/" className="nav-list">
+              Home
+            </Link>
+
+            <Link to="/list-news" className="nav-list">
+              News
+            </Link>
+
+            <Link to="/list-tips" className="nav-list">
+              Tips
+            </Link>
+
+            <Link to="#action1" className="nav-list">
+              Program
+            </Link>
+          </Nav>
+          <div className="d-flex gap-2 btn-nav">
+            <Button variant="outline-success" className="w-100 btn-login">
+              Login
+            </Button>
+            <Button variant="outline-success" className="w-100 btn-register">
+              Register
+            </Button>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
