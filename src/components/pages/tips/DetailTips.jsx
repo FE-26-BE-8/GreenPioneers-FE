64bb16ad0f32b563f86213c0dbab7b4e7917e533
KhathirulSan <paturihsan4@gmail.com>
2023-06-10 22:24:14 +0700

    Memindahkan DetailTips ke dalam page tips

---
+++ src/components/pages/tips/DetailTips.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "../css/DetailTips.css";
+
+function DetailTips() {
+  const [detailTips, setDetailTips] = useState([
+    {
+      id: 1,
+      imageSrc: "https://i.ibb.co/rb8r49W/image-2023-06-05-102713445.png",
+      title:
+        "Tips untuk Mengatasi Perubahan Iklim dan Memberikan Dampak Positif",
+      description:
+        "Di tengah dunia yang terus berubah, menangani perubahan iklim menjadi sangat penting. Artikel ini memberikan lima tips praktis untuk mengatasi perubahan iklim dan berkontribusi pada masa depan yang lebih berkelanjutan. Temukan langkah-langkah nyata yang dapat Anda lakukan dalam kehidupan sehari-hari untuk memberikan dampak positif pada lingkungan. Mulai dari mengurangi jejak karbon Anda hingga mendukung energi terbarukan, tips ini memberdayakan individu untuk menjadi agen perubahan dalam perjuangan melawan perubahan iklim. Bergabunglah dengan gerakan global dan ambil tindakan sekarang juga!",
+    },
+  ]);
+
+  return (
+    <>
+      <div className="container">
+        {detailTips.map((detailTip) => (
+          <div className="tips" key={detailTip.id}>
+            <h2 className="title">{detailTip.title}</h2>
+            <img src={detailTip.imageSrc} alt="Gambar Tips" />
+            <p>{detailTip.description}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default DetailTips;
