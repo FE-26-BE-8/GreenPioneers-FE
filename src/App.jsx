62cf3fb5b8b11d17afdff116af67bc48d930629d
KhathirulSan <paturihsan4@gmail.com>
2023-06-10 15:25:20 +0700

    Menambahkan navigasi Tips

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import Tips from "./components/pages/tips/Tips"
-import Home from "./components/pages/home/Home"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import ContactForm from "./components/pages/contact/ContactForm"
+import Tips from "./components/pages/tips/Tips";
+import Home from "./components/pages/home/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ContactForm from "./components/pages/contact/ContactForm";
 function App() {
   const router = createBrowserRouter([
     {
-      path: "/", 
-      element: <Home />
+      path: "/",
+      element: <Home />,
     },
     {
-      path: "/kontak", 
-      element: <ContactForm />
+      path: "/kontak",
+      element: <ContactForm />,
+    },
+    {
+      path: "/list-tips",
+      element: <Tips />,
     }
-  ])
+  ]);
   return (
     <>
-   
-  <RouterProvider router = {router}/>
-  </>
-  )
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
-export default App
+export default App;
