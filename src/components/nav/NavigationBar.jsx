920f4f5c58df969aa9a4798efabef5147923f836
KhathirulSan <paturihsan4@gmail.com>
2023-06-12 22:55:08 +0700

    Menavigasikan tombol login dan register

--- src/components/nav/NavigationBar.jsx
+++ src/components/nav/NavigationBar.jsx
@@ -2,22 +2,26 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
-import "./Navbar.css"
+import { Link, useNavigate } from "react-router-dom";
+import "./Navbar.css";
 function NavigationBar() {
+  const navigate = useNavigate();
+  function handleButton(url) {
+    navigate(url);
+  }
   return (
     <>
-    <Navbar className="container-nav" expand="lg">
-      <Container fluid>
-        <Navbar.Brand>
-          <div className="nav-logo">
-          <div className="logo-name1">
-            <a href="#">Green</a>
-          </div>
-          <div className="logo-name2">
-            <a href="#">Pioneers</a>
-          </div>
-        </div>
+      <Navbar className="container-nav" expand="lg">
+        <Container fluid>
+          <Navbar.Brand>
+            <div className="nav-logo">
+              <div className="logo-name1">
+                <a href="#">Green</a>
+              </div>
+              <div className="logo-name2">
+                <a href="#">Pioneers</a>
+              </div>
+            </div>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
@@ -26,15 +30,34 @@ function NavigationBar() {
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-         
-              <Link to ="/" className="m-auto nav-list active">Home</Link>
-              <Link to ="/list-news" className="m-auto nav-list active">News</Link>
-              <Link to ="/list-tips" className="m-auto nav-list active">Tips</Link>
-              <Nav.Link href="#action1"  className="m-auto nav-list active">Program</Nav.Link>
+              <Link to="/" className="m-auto nav-list active">
+                Home
+              </Link>
+              <Link to="/list-news" className="m-auto nav-list active">
+                News
+              </Link>
+              <Link to="/list-tips" className="m-auto nav-list active">
+                Tips
+              </Link>
+              <Nav.Link href="#action1" className="m-auto nav-list active">
+                Program
+              </Nav.Link>
             </Nav>
             <div className="d-flex gap-2 btn-nav">
-              <Button variant="outline-success" className="w-100 btn-login">Login</Button>
-              <Button variant="outline-success" className="w-100 btn-register">Register</Button>
+              <Button
+                onClick={() => handleButton("/login")}
+                variant="outline-success"
+                className="w-100 btn-login"
+              >
+                Login
+              </Button>
+              <Button
+                onClick={() => handleButton("/register")}
+                variant="outline-success"
+                className="w-100 btn-register"
+              >
+                Register
+              </Button>
             </div>
           </Navbar.Collapse>
         </Container>
