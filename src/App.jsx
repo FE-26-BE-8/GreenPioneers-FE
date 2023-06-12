51c8d639c4f3401a07f44a2fcca5bfccbb644095
KhathirulSan <paturihsan4@gmail.com>
2023-06-12 17:21:44 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import Footer from "./components/footer/Footer"
-import NavigationBar from "./components/nav/NavigationBar"
-import Home from "./components/pages/home/Home"
-
+import News from "./components/pages/news/News"
+import Tips from "./components/pages/tips/Tips";
+import Home from "./components/pages/home/home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ContactForm from "./components/pages/contact/ContactForm";
 function App() {
+  const router = createBrowserRouter([
+    {	
+      path: "/",	
+      element: <Home />,	
+    },	
+    {	
+      path: "/kontak",	
+      element: <ContactForm />,	
+    },	
+    {	
+      path: "/list-tips",	
+      element: <Tips />,	
+    },	
+    {	
+      path: "/list-news",	
+      element: <News />,	
+    }	
+  ]); 
   return (
     <>
-  <NavigationBar />
-  <Home />
-  <Footer />
+     <RouterProvider router={router} />
+    </>
 
-  </>
-  )
+  );
 }
-
-export default App
+export default App;
