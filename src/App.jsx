b11c256e93d9ea8c7bf2fb0223b9286584658f53
KhathirulSan <paturihsan4@gmail.com>
2023-06-12 15:42:42 +0700

    Memperbaiki tampilan single tips

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
+import Footer from "./components/footer/Footer"
+import NavigationBar from "./components/nav/NavigationBar"
 import Tips from "./components/pages/tips/Tips"
-import Home from "./components/pages/home/Home"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import ContactForm from "./components/pages/contact/ContactForm"
+
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/", 
-      element: <Home />
-    },
-    {
-      path: "/kontak", 
-      element: <ContactForm />
-    }
-  ])
   return (
     <>
-   
-  <RouterProvider router = {router}/>
+  <NavigationBar />
+  <Tips />
+  <Footer />
   </>
   )
 }
