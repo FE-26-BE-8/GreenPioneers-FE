1409a1e5372f428c5dd25c7ba12d1d55356cd074
KhathirulSan <paturihsan4@gmail.com>
2023-06-13 14:02:45 +0700

    menambahkan loading text

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -4,7 +4,12 @@ import './Loading.css'; // Import file CSS untuk styling
 const Loading = () => {
   return (
     <div className="loading-container">
-      <div className="loading-spinner"></div>
+      <div>
+        <div className="loading-spinner"></div>
+        </div>
+      <div className='loading-text'>
+      <h3>Harap Sabar ini Ujian..</h3>
+      </div>
     </div>
   );
 };
