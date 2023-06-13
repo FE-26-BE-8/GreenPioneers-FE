e7b2279e495ff715eaf939e18ea56ed5b66e4a50
KhathirulSan <paturihsan4@gmail.com>
2023-06-13 13:27:01 +0700

    Menambahkan fitur loading

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loading.css'; // Import file CSS untuk styling
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+};
+
+export default Loading;
--- src/components/pages/news/News.jsx
+++ src/components/pages/news/News.jsx
@@ -3,18 +3,23 @@ import NavigationBar from "../../nav/NavigationBar";
 import React, { useEffect, useState } from 'react';
 import Carousel from "react-bootstrap/Carousel";
 import UseApiCall from "../../../helper/UseApiCall";
+import Loading from "../../loading/Loading"
 import "./News.css";
-import axios from "axios";
+
 
 function News() {
 
   const {data, fetchData, error} = UseApiCall();
   const [newsData, setNewsData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true); // Tambahkan state isLoading
 
   useEffect(() => {
-    fetchData("https://pear-vast-bream.cyclic.app/api/news", "get", null, {'authorization': "Bearer "+ localStorage.getItem("Authorization") } ).then(data => setNewsData(data.data.data.news))
-
-  },[])
+    fetchData("https://pear-vast-bream.cyclic.app/api/news", "get", null, {
+      'authorization': "Bearer " + localStorage.getItem("Authorization")
+    }).then(data => {setNewsData(data.data.data.news);
+      setIsLoading(false); // Set isLoading ke false setelah mendapatkan data
+    });
+  }, []);
 
   useEffect(() => {
     console.log(newsData)
@@ -71,52 +76,66 @@ function News() {
    <div className="col-md-7">
      <h1 className="heading">News</h1>
 
-     {newsData ? newsData.map((news) => (
+     {isLoading ? (
+  <Loading /> // Tampilkan Loading jika isLoading true
+) : (
+  newsData ? (
+    newsData.map((news) => (
+      <div className="card mb-3 p-4 news-card" key={news.id}>
+        <div className="row g-0">
+          <div className="col-md-6">
+            <h5 className="card-title">{news.judul}</h5>
+            <p className="card-text">{news.isi}</p>
+            <div className="text-center btn-article">
+              <button className="btn-news">Baca Selengkapnya</button>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card-body">
+              <img
+                src={news.gambar}
+                alt="Kebakaran Hutan"
+                className="img-fluid img-news"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    ))
+  ) : (
+    <h1>Data News Tidak Ditemukan</h1>
+  )
+)}
 
-       <div className="card mb-3 p-4 news-card" key={news.id}>
-         <div className="row g-0">
-           <div className="col-md-6">
-             <h5 className="card-title">{news.judul}</h5>
-             <p className="card-text">{news.isi}</p>
-             <div className="text-center btn-article">
-               <button className="btn-news">Baca Selengkapnya</button>
-             </div>
-           </div>
-           <div className="col-md-6">
-             <div className="card-body">
-               <img
-                 src={news.gambar}
-                 alt="Kebakaran Hutan"
-                 className="img-fluid img-news"
-               />
-             </div>
-           </div>
-         </div>
-       </div>
-     ) ) : <h1>Data News Tidak Ditemukan</h1> }
    </div>
 
    {/* //---------------------Trending Topics-------------------------- */}
    <div className="col-md-4 mt-lg-5 trend-container">
    <div className="trending-topics">
-     <h2>Trending Topics</h2>
-     <ul>
-       {newsData ? newsData.map((topic, index) => (
-         <li key={index}>
-           <div className="topic">
-             <img src={topic.gambar} alt="Trending Topic" />
-             <div>
-               <a href="#">{topic.judul}</a>
-               <p className="card-text">
-                 <small className="text-body-secondary">
-                   {topic.waktu}
-                 </small>
-               </p>
-             </div>
-           </div>
-         </li>
-       ) ) : <h1>Data Trending Tidak Ditemukan</h1> }
-     </ul>
+   <h2>Trending Topics</h2>
+   {isLoading ? (
+      <Loading /> // Tampilkan Loading jika isLoading true
+    ) : (
+      <ul>
+        {newsData ? (
+          newsData.map((topic, index) => (
+            <li key={index}>
+              <div className="topic">
+                <img src={topic.gambar} alt="Trending Topic" />
+                <div>
+                  <a href="#">{topic.judul}</a>
+                  <p className="card-text">
+                    <small className="text-body-secondary">{topic.waktu}</small>
+                  </p>
+                </div>
+              </div>
+            </li>
+          ))
+        ) : (
+          <h1>Data Trending Tidak Ditemukan</h1>
+        )}
+      </ul>
+    )}
    </div>
  </div>
  </div>
