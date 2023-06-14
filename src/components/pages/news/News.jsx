f4362d1294e7da1e83691c74e0889d77fc863392
KhathirulSan <paturihsan4@gmail.com>
2023-06-14 16:46:54 +0700

    .

--- src/components/pages/news/News.jsx
+++ src/components/pages/news/News.jsx
@@ -66,7 +66,10 @@ function News() {
           <div className="wrapper-title-carousel">
             <div className="title-carousel">
               <h3>Melawan Nyala Membara</h3>
-              <p>Melintasi Awan Asap, Membuka Mata Hati pada Penderitaan Hutan yang Terbakar.</p>
+              <p>
+                Melintasi Awan Asap, Membuka Mata Hati pada Penderitaan Hutan
+                yang Terbakar.
+              </p>
             </div>
           </div>
         </Carousel.Item>
@@ -79,7 +82,10 @@ function News() {
           <div className="wrapper-title-carousel">
             <div className="title-carousel">
               <h3>Kekeringan</h3>
-              <p>Ketika Pohon-pohon Tertunduk dan Hati-hati Merana dalam Rindu Akan Air</p>
+              <p>
+                Ketika Pohon-pohon Tertunduk dan Hati-hati Merana dalam Rindu
+                Akan Air
+              </p>
             </div>
           </div>
         </Carousel.Item>
@@ -99,74 +105,78 @@ function News() {
       </Carousel>
 
       {/* //---------------------NEWS-------------------------- */}
-      <div className="background-container-2"> 
-      <div className="news-container container">
-        <div className="col-md-7">
-          <h1 className="heading">News</h1>
+      <div className="background-container-2">
+        <div className="news-container container">
+          <div className="col-md-7">
+            <h1 className="heading">News</h1>
 
-          {isLoading ? (
-            <Loading />
-          ) : newsData ? (
-            newsData.map((news) => (
-              <div className="card mb-3 p-4 news-card" key={news.id}>
-                <div className="row g-0">
-                  <div className="col-md-6">
-                    <h5 className="card-title">{news.judul}</h5>
-                    <p className="card-text">{truncateDescription(news.isi, 30)}</p>
-                    <div className="text-center btn-article">
-                      <button
-                        onClick={() => handleDetailButton(news.id)}
-                        className="btn-news"
-                      >
-                        Baca Selengkapnya
-                      </button>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="card-body">
-                      <img
-                        src={news.gambar}
-                        alt="Kebakaran Hutan"
-                        className="img-fluid img-news"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))
-          ) : (
-            <h1>Data News Tidak Ditemukan</h1>
-          )}
-        </div>
-
-        {/* //---------------------Trending Topics-------------------------- */}
-        <div className="col-md-4 mt-lg-5 trend-container">
-          <div className="trending-topics">
-            <h2>Trending Topics</h2>
             {isLoading ? (
               <Loading />
             ) : newsData ? (
-              <ul>
-                {newsData.map((topic, index) => (
-                  <li key={index}>
-                    <div className="topic">
-                      <img src={topic.gambar} alt="Trending Topic" />
-                      <div>
-                        <a href="#">{topic.judul}</a>
-                        <p className="card-text">
-                          <small className="text-body-secondary">{topic.waktu}</small>
-                        </p>
+              newsData.map((news) => (
+                <div className="card mb-3 p-4 news-card" key={news.id}>
+                  <div className="row g-0">
+                    <div className="col-md-6">
+                      <h5 className="card-title">{news.judul}</h5>
+                      <p className="card-text">
+                        {truncateDescription(news.isi, 30)}
+                      </p>
+                      <div className="text-center btn-article">
+                        <button
+                          onClick={() => handleDetailButton(news.id)}
+                          className="btn-news"
+                        >
+                          Baca Selengkapnya
+                        </button>
                       </div>
                     </div>
-                  </li>
-                ))}
-              </ul>
+                    <div className="col-md-6">
+                      <div className="card-body">
+                        <img
+                          src={news.gambar}
+                          alt="Kebakaran Hutan"
+                          className="img-fluid img-news"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))
             ) : (
-              <h1>Data Trending Tidak Ditemukan</h1>
+              <h1>Data News Tidak Ditemukan</h1>
             )}
           </div>
+
+          {/* //---------------------Trending Topics-------------------------- */}
+          <div className="col-md-4 mt-lg-5 trend-container">
+            <div className="trending-topics">
+              <h2>Trending Topics</h2>
+              {isLoading ? (
+                <Loading />
+              ) : newsData ? (
+                <ul>
+                  {newsData.map((topic, index) => (
+                    <li key={index}>
+                      <div className="topic">
+                        <img src={topic.gambar} alt="Trending Topic" />
+                        <div>
+                          <a href="#">{topic.judul}</a>
+                          <p className="card-text">
+                            <small className="text-body-secondary">
+                              {topic.waktu}
+                            </small>
+                          </p>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <h1>Data Trending Tidak Ditemukan</h1>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
       </div>
       <Footer />
     </>
