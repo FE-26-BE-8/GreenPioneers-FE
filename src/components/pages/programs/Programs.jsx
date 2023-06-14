d4b4d59d9916cefe45c0e56ac03bec3a064df243
KhathirulSan <paturihsan4@gmail.com>
2023-06-14 16:03:36 +0700

    Merapihkan kodingan Programs

--- src/components/pages/programs/Programs.jsx
+++ src/components/pages/programs/Programs.jsx
@@ -6,16 +6,18 @@ import UseApiCall from "../../../helper/UseApiCall";
 import "./Programs.css";
 
 function Programs() {
-  const {data, fetchData, error} = UseApiCall();
+  const { data, fetchData, error } = UseApiCall();
   const [programs, setPrograms] = useState(null);
 
   useEffect(() => {
-    fetchData("https://pear-vast-bream.cyclic.app/api/program", "get", null, {'authorization': "Bearer "+ localStorage.getItem("Authorization") } ).then(data => setPrograms(data.data.data.program))
-  },[])
+    fetchData("https://pear-vast-bream.cyclic.app/api/program", "get", null, {
+      authorization: "Bearer " + localStorage.getItem("Authorization"),
+    }).then((data) => setPrograms(data.data.data.program));
+  }, []);
 
   useEffect(() => {
-    console.log(programs)
-  },[programs])
+    console.log(programs);
+  }, [programs]);
 
   const truncateDescription = (description, maxWords) => {
     const words = description.split(" ");
@@ -25,12 +27,11 @@ function Programs() {
     return description;
   };
 
-  
   return (
     <>
       <NavigationBar />
       {/* //---------------------CarouselPrograms-------------------------- */}
-    <Carousel className="programs-carousel">
+      <Carousel className="programs-carousel">
         <Carousel.Item>
           <img
             className="d-block w-100"
@@ -53,7 +54,10 @@ function Programs() {
           <div className="wrapper-title-carousel">
             <div className="title-carousel">
               <h3>Kami Membutuhkanmu</h3>
-              <p>Bergabunglah dalam Program Peduli Lingkungan <br/> dan Bersama Kita Jadikan Bumi Tempat yang Lebih Baik!</p>
+              <p>
+                Bergabunglah dalam Program Peduli Lingkungan <br /> dan Bersama
+                Kita Jadikan Bumi Tempat yang Lebih Baik!
+              </p>
             </div>
           </div>
         </Carousel.Item>
@@ -66,45 +70,54 @@ function Programs() {
           <div className="wrapper-title-carousel">
             <div className="title-carousel">
               <h3>Satu Aksi, Satu Perubahan</h3>
-              <p>Jadilah Bagian dari Program Peduli Lingkungan dan Bantu Lindungi Alam! </p>
+              <p>
+                Jadilah Bagian dari Program Peduli Lingkungan dan Bantu Lindungi
+                Alam!{" "}
+              </p>
             </div>
           </div>
         </Carousel.Item>
       </Carousel>
 
-        {/* //---------------------Programs-------------------------- */}
-
+      {/* //---------------------Programs-------------------------- */}
+      <div className="background-container"> 
       <div className="ajakan">
-        <h1>Ayo, mari ikut serta dalam program tentang perubahan iklim dan berkontribusi dalam menjaga keberlanjutan bumi kita bersama!</h1>
+        <h1>
+          Ayo! mari ikut serta dalam program tentang perubahan iklim dan
+          berkontribusi dalam menjaga keberlanjutan bumi kita bersama!
+        </h1>
       </div>
       <div className="programs-container">
         <div className="container">
           <h1 className="program-heading">Program Kami</h1>
         </div>
         <div className="center-container">
-           { programs ? programs.map((program) => (
-            <div className="program-container" key={program.id}>
-              <div className="img-container">
-                <img
-                  src={program.gambar}
-                  alt={program.judul}
-                  className="program-image"
-                />
-              </div>
-              <div className="text-container">
-                <h2 className="program-title">{program.judul}</h2>
-                <p className="program-description">
-                  {truncateDescription(program.isi, 12)}
-                </p>
-                <div className="button-container">
-                  <button className="follow-button">Ikuti Program</button>
+          {programs ? (
+            programs.map((program) => (
+              <div className="program-container" key={program.id}>
+                <div className="img-container">
+                  <img
+                    src={program.gambar}
+                    alt={program.judul}
+                    className="program-image"
+                  />
+                </div>
+                <div className="text-container">
+                  <h2 className="program-title">{program.judul}</h2>
+                  <p className="program-description">
+                    {truncateDescription(program.isi, 12)}
+                  </p>
+                  <div className="button-container">
+                    <button className="btn-event">Ikuti Program</button>
+                  </div>
                 </div>
               </div>
-            </div>
-          )): (
+            ))
+          ) : (
             <h1>Data Tidak Ditemukan</h1>
           )}
         </div>
+      </div>
       </div>
       <Footer />
     </>
