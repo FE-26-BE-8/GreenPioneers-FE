16540921b0c9ddd2e7f71ad1fdd2a4f8b817e216
KhathirulSan <paturihsan4@gmail.com>
2023-06-15 00:44:13 +0700

    menambahkan loading serta lainnya

--- src/components/pages/news/DetailNews.jsx
+++ src/components/pages/news/DetailNews.jsx
@@ -1,33 +1,54 @@
+import Footer from "../../footer/Footer";
+import NavigationBar from "../../nav/NavigationBar";
 import React, { useState, useEffect } from "react";
 import UseApiCall from "../../../helper/UseApiCall";
-import "./DetailNews.css";
+import Loading from "../../loading/Loading";
 import { useParams } from "react-router-dom";
+import "./DetailNews.css";
 
 function DetailNews() {
   const { id } = useParams();
   const { fetchData, error } = UseApiCall();
   const [detailNews, setDetailNews] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetchData(`https://pear-vast-bream.cyclic.app/api/news/${id}`, "get", null, {
-      authorization: "Bearer " + localStorage.getItem("Authorization"),
-    }).then((data) => setDetailNews(data.data.data.news));
+    fetchData(
+      `https://pear-vast-bream.cyclic.app/api/news/${id}`,
+      "get",
+      null,
+      {
+        authorization: "Bearer " + localStorage.getItem("Authorization"),
+      }
+    )
+      .then((data) => {
+        setDetailNews(data.data.data.news);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+      });
   }, []);
 
   return (
-    <div className="container">
-      {detailNews ? (
-        <div className="news" key={detailNews.id}>
-        <h2 className="title">{detailNews.judul}</h2>
-        <img src={detailNews.gambar} alt="Gambar News" />
-        <p>{detailNews.isi}</p>
-      </div>
+    <>
+      <NavigationBar />
+      <div className="container">
+        {isLoading ? (
+          <Loading />
+        ) : detailNews ? (
+          <div className="news" key={detailNews.id}>
+            <h2 className="title">{detailNews.judul}</h2>
+            <img src={detailNews.gambar} alt="Gambar News" />
+            <p>{detailNews.isi}</p>
+          </div>
         ) : (
-        <h1>Data Tidak Ditemukan!</h1>
-      )}
-    </div>
+          <h1>Data Tidak Ditemukan!</h1>
+        )}
+      </div>
+      <Footer />
+    </>
   );
 }
 
 export default DetailNews;
-
